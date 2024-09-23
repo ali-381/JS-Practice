a5b5cb45dc11b7f21bac80adf7f7c3f047bf79e6
ali-381 <ali.mnaeem381@gmail.com>
2024-09-23 17:30:46 +0500

    add a new js file

---
+++ Object.js
@@ -0,0 +1,16 @@
+// alert('hello guys')
+
+let DATA = "Some Secret information";
+
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  viewData() {
+    console.log("apna colleg");
+  }
+}
+
+let Stud1 = new User("Ali", "ali@gmail.com");
+let Stud2 = new User("Danish", "danish@gmail.com");
